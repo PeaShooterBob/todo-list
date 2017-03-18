ec970865df5bbe12ef607a6ffd835a9f9c429941
Robert Gu <robertgu84@gmail.com>
2017-03-18 13:16:18 -0400

    Validates uniqueness of project title

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ActiveRecord::Base
-  validates :title, :presence => true
+  validates :title, 
+    :presence => true, 
+    :uniqueness => {:case_sensitive => false}
   has_many :items, :dependent => :destroy
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -20,5 +20,24 @@ RSpec.describe Project, :type => :model do
       expect(subject).to_not be_valid
       expect(subject.errors.keys).to eq [:title]
     end
+
+    it "requires title be unique within a project" do
+      subject.save
+
+      duplicate_action = Project.new(params)
+
+      expect(duplicate_action).to_not be_valid
+      expect(duplicate_action.errors.keys).to include :title
+    end
+
+    it "requires title to be unique ignoring case" do
+      subject.save
+
+      params[:title] = params[:title].downcase
+      duplicate_action = Project.new(params)
+
+      expect(duplicate_action).to_not be_valid
+      expect(duplicate_action.errors.keys).to include :title
+    end
   end
 end
