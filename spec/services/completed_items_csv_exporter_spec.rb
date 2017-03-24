2f2687058219aeec03aa97cacc97db0e733b63f5
Robert Gu <robertgu84@gmail.com>
2017-03-24 15:09:17 -0700

    Created at added to CSV exporter

--- spec/services/completed_items_csv_exporter_spec.rb
+++ spec/services/completed_items_csv_exporter_spec.rb
@@ -4,11 +4,13 @@ describe CompletedItemsCsvExporter do
   let(:project_title) { 'Awesome Project' }
   let(:updated_date) { Date.current }
   let(:action) { 'do something' }
+  let(:date_added) { Date.current - 2.days }
   let(:item1) do
     instance_double(Item,
                     :project_title => project_title,
                     :updated_at => updated_date,
-                    :action => action)
+                    :action => action,
+                    :created_at => date_added)
   end
   let(:item_relation) { [item1] }
   let(:io_object) do
@@ -34,7 +36,7 @@ describe CompletedItemsCsvExporter do
   end
 
   describe '#export!' do
-    let(:header) { "Project Title,Action,Date Completed\n" }
+    let(:header) { "Project Title,Action,Date Completed,Date Added\n" }
 
     it 'writes header' do
       expect(io_object).to receive(:<<).with(header)
@@ -43,7 +45,7 @@ describe CompletedItemsCsvExporter do
 
     it 'writes expected completed item lines to file' do
       expect(io_object).to receive(:<<).with(
-        "#{project_title},#{action},#{updated_date}\n"
+        "#{project_title},#{action},#{updated_date},#{date_added}\n"
       )
       exporter.export!
     end
